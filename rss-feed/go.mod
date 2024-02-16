d1d3a19a9d0eaaeb09e7476f05466c4dd8d7d0cd
Rohith Raju <rohith.raju@ecarxgroup.com>
2024-02-16 14:48:16 +0100

    Adding cors and thunder client for request for rss feed
    Issue: NONE

--- rss-feed/go.mod
+++ rss-feed/go.mod
@@ -1,3 +1,8 @@
 module rssagg
 
 go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- rss-feed/main.go
+++ rss-feed/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hi Rss feed")
+
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in env")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:  []string{"https://*", "http://*"},
+		AllowedMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:  []string{"*"},
+		ExposedHeaders:  []string{"Link"},
+		AllowCredetials: false,
+		MaxAge:          300,
+	}))
+
+	httpServer := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("server starting on port %v", portString)
+	err := httpServer.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
